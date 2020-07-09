9bf7327a69709e32d59b86508e6da7b0913bbadd
josePereiro <jose.pereiro.lab@gmail.com>
2020-07-09 13:26:14 -0400

    Reduce workers num

--- scripts/Human1/4_fva_pp_base_model_maxent_ep.jl
+++ scripts/Human1/4_fva_pp_base_model_maxent_ep.jl
@@ -40,13 +40,8 @@ Chemostat_Rath2017.check_env();
 using Distributed
 
 NO_CORES = length(Sys.cpu_info())
-length(workers()) < NO_CORES - 1 && addprocs(NO_CORES - 1; 
+length(workers()) < NO_CORES - 2 && addprocs(NO_CORES - 2; 
     exeflags = "--project")
-# atexit() do
-#     for w in workers()
-#         remote_do(exit, w)
-#     end
-# end
 println("Working in: ", workers())
 
 # +
