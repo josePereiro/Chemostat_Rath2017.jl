6034ee0aeb6790d4d8b88efba107f2c9281de63f
josePereiro <jose.pereiro.lab@gmail.com>
2020-07-16 12:56:15 -0400

    Rename the scripts, add runall

--- scripts/Human1/HumanGEM/0_runall.jl
+++ scripts/Human1/HumanGEM/0_runall.jl
@@ -5,20 +5,41 @@
 cd(dirname(@__FILE__)) # move to this folder
 println("Now at: ", pwd())
 
-
 # Scripts in order
-scripts = [
-    "1_Hams_medium.jl",
-    "1_mets_map.jl",
-    "1_niklas_biomass.jl",
-    "2_prepare_base_model.jl",
-    # "3_prepare_fva_pp_model.jl",
-    # "4_fva_pp_base_model_maxent_ep.jl",
-    # "5_fva_pp_base_model_maxent_ep_plots.jl"
+
+base_scripts = [
+    "download_raws.jl",
+    "Hams_medium.jl",
+    "mets_map.jl",
+    "niklas_biomass.jl",
+    "prepare_base_model.jl",
 ]
 
-for script in scripts
-    
+all_scripts = [
+    base_scripts;
+    "prepare_fva_pp_model.jl";
+    "fva_pp_base_model_maxent_ep.jl";
+    "fva_pp_base_model_maxent_ep_plots.jl"
+]
+
+usage = "If zero argument are passed, it will run 'all' the scripts. "*
+    "Possible option 'all', 'base'."
+if isempty(ARGS)
+    to_run = all_scripts
+elseif length(ARGS) == 1
+    if ARGS[1] == "base"
+        to_run = base_scripts
+    elseif ARGS[1] == "all"
+        to_run = all_scripts
+    else
+        error("$(ARGS[1]) not recognized!!\nUsage: $usage")
+    end
+else
+    error("To many args!!\nUsage: $usage")
+end
+
+println("To run: ", to_run)
+for script in to_run
     println("\n\n----------------- Running $script -----------------\n\n")
     # run(`$julia_cmd $script`)
     run(`julia --project $script`) # Change this to point to your julia executable, do not forget the flag
---
+++ scripts/Human1/HumanGEM/download_raws.jl
@@ -0,0 +1,21 @@
+# +
+import CSV
+import DataFrames: DataFrame
+import MAT
+
+import Chemostat_Rath2017
+HG = Chemostat_Rath2017.HumanGEM
+# -
+
+# This just check that the script is run in the
+# package enviroment
+Chemostat_Rath2017.check_env();
+
+zenodo_download_link = "https://zenodo.org/record/3583004/files/Human1_Publication_Data_Scripts.zip?download=1"
+
+if isdir(HG.MODEL_RAW_DATA_DIR)
+    println(relpath(HG.MODEL_RAW_DATA_DIR), " already exist, to force a re-download delete the folder")
+else
+    # download(zenodo_download_link, HG.MODEL_RAW_DATA_DIR)
+    println(relpath(HG.MODEL_RAW_DATA_DIR), " downloaded")
+end
