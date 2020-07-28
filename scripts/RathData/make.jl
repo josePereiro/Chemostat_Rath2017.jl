04df5de8d85913e1275da11c2867d35356fe3e02
josePereiro <jose.pereiro.lab@gmail.com>
2020-07-28 00:07:28 -0400

    Add RathData make.jl

---
+++ scripts/RathData/make.jl
@@ -0,0 +1,105 @@
+#=
+    This script just run all the other scripts in the correct order.
+=#
+#######################################################
+## SET UP
+#######################################################
+using ArgParse
+set = ArgParseSettings()
+@add_arg_table! set begin
+    "--dry-run"
+        help = "run without consecuences, just printing"
+        action = :store_true
+    "--run", "-r", "-x"
+        help = "run all the scripts"
+        action = :store_true
+        default = true
+    "--clear", "-c"
+        help = "Clear first all the scripts target files"
+        action = :store_true
+end
+parsed_args = parse_args(set)
+dry_run_flag = parsed_args["dry-run"]
+clear_arg = parsed_args["clear"]
+run_arg = parsed_args["run"]
+
+cd(dirname(@__FILE__)) # move to this folder
+println("\nNow at: ", pwd())
+
+import Chemostat_Rath2017
+Rd = Chemostat_Rath2017.RathData;
+
+# This just check that the script is run in the
+# package enviroment
+Chemostat_Rath2017.check_env();
+
+# Scripts-targets in order
+all_scripts = [
+    (
+        name = "convert_rath_data.jl",
+        targets = joinpath.(Rd.RATH_PROCESSED_DATA_DIR, 
+            ["rath2017___42_MAX_UB_standard_medium.tsv", "rath2017___cont_exp_A.tsv",
+            "rath2017___cont_exp_B.tsv", "rath2017___cont_exp_C.tsv", 
+            "rath2017___cont_exp_D.tsv", "rath2017___cont_exp_E.tsv",
+            "rath2017___cont_exp_F01.tsv", "rath2017___cont_exp_F02.tsv",
+            "rath2017___cont_exp_F03.tsv", "rath2017___max_invitro_fluxs.tsv"])
+    ),
+    (
+        name = "a1at_demand.jl",
+        targets = joinpath.(Rd.RATH_PROCESSED_DATA_DIR, 
+            ["a1at_aa_rel_abundance.csv"])
+    )
+] 
+
+get_names(scripts) = [basename(script.name) for script in scripts]
+get_script(name) = all_scripts[get_names(all_scripts) .== name]
+
+#######################################################
+## CLEAR
+#######################################################
+if clear_arg
+
+    to_clear = all_scripts
+    println("\nTo clear: ", get_names(to_clear))
+    for (script, targets) in to_clear 
+        for target in targets
+            if isfile(target) || isdir(target)
+                !dry_run_flag && rm(target, force = true, recursive = true)
+                println(basename(target), " deleted!!!")
+            end
+        end
+    end
+    flush(stdout); flush(stderr)
+end
+
+#######################################################
+## RUN SCRIPTS
+#######################################################
+
+function check_targets(targets) 
+    if isempty(targets)
+        println("Running: No targets set\n")
+        return false
+    end
+    for target in targets
+        if !isfile(target) && !isdir(target)
+            println("Running: ", basename(target), " missing\n")
+            return false
+        end
+    end
+    println("Not running: All targets present\n")
+    return true
+end
+
+if run_arg
+    to_run = all_scripts
+    julia = Base.julia_cmd()
+    println("\nTo run: ", get_names(to_run))
+    for (script, targets) in to_run
+        println("\n\n----------------- Script $script -----------------\n")
+        check_targets(targets) && continue
+        flush(stdout); flush(stderr)
+        !dry_run_flag && run(`$julia --project $script`)
+        flush(stdout); flush(stderr)
+    end
+end
