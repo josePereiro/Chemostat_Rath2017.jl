798f91eadf55e2b6ce516d6ceb3f53a352a38069
josePereiro <jose.pereiro.lab@gmail.com>
2020-08-09 18:03:57 -0400

    Update Project

--- src/Human1/tINIT_GEMs/tINIT_GEMs.jl
+++ src/Human1/tINIT_GEMs/tINIT_GEMs.jl
@@ -4,7 +4,7 @@
 # to reproduce the models, analyses, and figures are available on Zenodo (https://doi.org/10.5281/zenodo.3577466). 
 
 module tINIT_GEMs
-import ..Chemostat_Rath2017: PROJ_ROOT, RAW_DATA_DIR, PROCESSED_DATA_DIR, FIGURES_DATA_DIR, RathData
+    import ..Chemostat_Rath2017: PROJ_ROOT, RAW_DATA_DIR, PROCESSED_DATA_DIR, FIGURES_DATA_DIR, RathData
     
     include("meta.jl")
     include("dir_and_files.jl")
