abdc32970505bb88bd513c9e15a5c824a826bf68
josePereiro <jose.pereiro.lab@gmail.com>
2020-10-08 14:16:02 -0400

    Add install script

---
+++ scripts/Remote/install.jl
@@ -0,0 +1,15 @@
+try; import DrWatson
+catch
+    import Pkg
+    Pkg.add("DrWatson")
+end
+import DrWatson: quickactivate
+quickactivate(@__DIR__, "Chemostat_Rath2017")
+
+## ------------------------------------------------------------------------
+using Pkg
+pkg"rm Chemostat"
+pkg"rm UtilsJL"
+pkg"add https://github.com/josePereiro/UtilsJL.git#v0.2.3"
+pkg"add https://github.com/josePereiro/Chemostat#v0.7.0"
+pkg"build"
