6e4b236e7e2b267debf3f8d89373059b6bb63d38
josePereiro <jose.pereiro.lab@gmail.com>
2021-02-14 08:35:30 -0500

    WIP, alpha Inf

--- scripts/Human1/HumanGEM/1_download_raws.jl
+++ scripts/Human1/HumanGEM/1_download_raws.jl
@@ -3,12 +3,16 @@ import DrWatson: quickactivate
 quickactivate(@__DIR__, "Chemostat_Rath2017")
 
 ## --------------------------------------------------------------------
-import Chemostat_Rath2017: HumanGEM
-HG = HumanGEM
+import Chemostat_Rath2017
+const ChR = Chemostat_Rath2017
+const HG = ChR.Human1.HumanGEM
 
-const zenodo_download_link = "https://zenodo.org/record/3583004/files/Human1_Publication_Data_Scripts.zip?download=1"
+## --------------------------------------------------------------------
+# const zenodo_download_link = "https://zenodo.org/record/3583004/files/Human1_Publication_Data_Scripts.zip?download=1"
+zenodo_download_link = "https://zenodo.org/record/3583004/files/Human1_Publication_Data_Scripts.zip"
 
 cd(HG.MODEL_RAW_DATA_DIR |> dirname)
+println("Now at: ", pwd())
 zip_file = "Human1_Publication_Data_Scripts.zip"
 if isdir(HG.MODEL_RAW_DATA_DIR)
     println(relpath(HG.MODEL_RAW_DATA_DIR), " already exist, to force a re-unzip delete the folder")
@@ -16,7 +20,8 @@ elseif isfile(zip_file)
     println(relpath(zip_file), " already exist, to force a re-download delete it")
     run(`unzip $zip_file`)
 else
-    download(zenodo_download_link, zip_file)
+    println("Downloading using curl ...")
+    run(`curl $(zenodo_download_link) --output $(zip_file)`)
     run(`unzip $zip_file`)
 end
 !isdir(HG.MODEL_RAW_DATA_DIR) && 
--- scripts/MODEL1105100000/4_run_maxent_fba_ep.jl
+++ scripts/MODEL1105100000/4_run_maxent_fba_ep.jl
@@ -30,7 +30,7 @@ else
     finish_clear_flag = parsed_args["clear-at-finish"]
 end
 
-## ----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 using Distributed
 
 NO_WORKERS = min(length(Sys.cpu_info()), wcount)
@@ -38,8 +38,8 @@ length(workers()) < NO_WORKERS &&
     addprocs(NO_WORKERS; exeflags = "--project")
 println("Working in: ", workers())
 
-## Loading everywhere
-@everywhere begin
+# Loading everywhere
+@time @everywhere begin
 
     import DrWatson: quickactivate
     quickactivate(@__DIR__, "Chemostat_Rath2017")
@@ -65,6 +65,8 @@ println("Working in: ", workers())
 
     import UtilsJL
     const UJL = UtilsJL
+
+    using Plots
     
     UJL.set_cache_dir(M.MODEL_CACHE_DATA_DIR)
 end
@@ -81,144 +83,333 @@ end
 # SIMULATION GLOBAL ID
 # This must uniquely identify this simulation version
 # It is used to avoid cache collisions
-@everywhere sim_global_id = "MAXENT_FBA_EP_v1"
+@everywhere FILE_ID = "4"
 
 ## ----------------------------------------------------------------------------
-# GET MODEL FUNCTION
+# UTILS
+@everywhere begin
+
+    function dat_file(name, ext = "jls"; kwargs...) 
+        fname = UJL.mysavename(name; A = 1)
+        joinpath(M.MODEL_PROCESSED_DATA_DIR, 
+            UJL.mysavename(string(FILE_ID, "_", name), ext; kwargs...)
+        )
+    end
+
+    FIG_DIR = joinpath(M.MODEL_FIGURES_DATA_DIR, "$(FILE_ID)_err_progress")
+
+    ME_BOUNDED = :ME_BOUNDED
+end
+mkpath(FIG_DIR)        
+
+## ----------------------------------------------------------------------------
+# AUX FUNCTIONS
 @everywhere function load_model(name, stst; compressed = false)
-    MINDEX = UJL.load_data(M.MODEL_INDEX_FILE)
+    MINDEX = UJL.load_data(M.MODEL_INDEX_FILE; verbose = false)
     mfile = MINDEX[stst][name]
     model = deserialize(mfile)
     compressed ? model : ChU.uncompressed_model(model)
 end
 
-## ----------------------------------------------------------------------------
-# RES IDS
-# Collect all the computed results ids for bundling
-const chnl = RemoteChannel() do
-    Channel{Any}(10)
-end
-const res_ids = []
-const collector = @async while true
-    id = take!(chnl)
-    push!(res_ids, id)
-end
+@everywhere function plot_progress(sim_id, stst, method, 
+        errs_tracking, max_beta_tracking
+    )
+    try
+        errs_len = length(errs_tracking)
+        curr_err = last(errs_tracking)
 
-## ----------------------------------------------------------------------------
-# BETA EXPLORATION SIMULATION
-# Any of the loops can be parallelized by just 
-# changing one of the 'map' functions
-
-# ststs = Rd.ststs
-ststs = ["E"] # Test
-pmap(ststs) do stst
-    
-    ## ----------------------------------------------------------------------------
-    ## SIMULATION PARAMS
-    βs = [0.0; UJL.logspace(1, 9, 1000)] 
-
-    # This determine how often EP results will be cached
-    epochlen = 30
-
-    epconv_kwargs = Dict()
-    # The maximum number of iteration before EP to return, even if not converged
-    epconv_kwargs[:maxiter] = Int(1e3) 
-    # The error threshold of convergence (Big at first)
-    epconv_kwargs[:epsconv] = 1e-4
-    epconv_kwargs[:damp] = 0.98
-    epconv_kwargs[:maxvar] = 1e50
-    epconv_kwargs[:minvar] = 1e-50
-
-    epmodel_kwargs = Dict()
-    epmodel_kwargs[:alpha] = Inf
-
-    # STOP CONDITION
-    comp_model = load_model(:scaled_fva_pp_model, stst; compressed = true)
-    exp_μ = Rd.val(:μ, stst)
-    μ_convth = 1e-2
-    
-
-    ## ----------------------------------------------------------------------------
-    # BETAS
-    sim_id = (stst, sim_global_id)
-    get_model() = load_model(:scaled_fva_pp_model, stst)
-
-    function on_betaiter(epout)
-        isnothing(epout) && return (false, epout)
-        
-        curr_μ = ChU.av(comp_model, epout, M.BIOMASS_IDER)
-        isnan(curr_μ) && error("curr_μ = NaN")
-        
-        err = abs(curr_μ - exp_μ)/ exp_μ
-        break_ = err < μ_convth || curr_μ > exp_μ
-
-        UJL.tagprintln_inmw("BEFORE EPOCH", 
+        UJL.tagprintln_inmw("PLOTTING", 
             "\nsim id:                      ", sim_id, 
-            "\ncurr_μ:                      ", curr_μ, 
-            "\nexp_μ:                       ", exp_μ, 
-            "\ngrowth err:                  ", err,
-            "\nbreak:                         ", break_,
+            "\nerrs_len:                    ", errs_len, 
             "\n"
         )
-        
-        return break_
-    end
 
-    sim_dat = ChSU.cached_simulation(;
-        sim_id, epochlen, 
-        verbose = true,
-        get_model, on_betaiter,
-        objider = M.BIOMASS_IDER, 
-        costider = M.COST_IDER,
-        beta_info = [(M.BIOMASS_IDER, βs)],
-        clear_cache = false, use_seed = true,
-        epmodel_kwargs, epconv_kwargs
+        p1 = plot(log10.(errs_tracking); title = "EP error evolution",
+            label = "", xlabel = "ep iteration", ylabel = "log(err)",
+            lw = 3
+        )
+        p2 = plot(log10.(max_beta_tracking); title = "EP beta evolution",
+            label = "", xlabel = "ep iteration", ylabel = "log(beta + eps)",
+            lw = 3
+        )
+
+        ps = Plots.Plot[p1, p2]
+        layout = (2, 1)
+        UJL.mysavefig(ps, "err_evolution", FIG_DIR; 
+            method, layout, stst, errs_len
+        )
+        UJL.mysavefig(ps, "tot_err_evolution", FIG_DIR; 
+            method, layout, stst
+        )
+
+    catch err; @warn("ERROR", stst, err); println() end
+end
+
+@everywhere function oniter(it, epmodel, sim_id, stst, method, 
+        errs_tracking, max_beta_tracking, plot_frec
     )
 
-    serialize("test_sim_dat.jld", sim_dat)
+    # Collect
+    push!(errs_tracking, epmodel.stat[:max_err])
+    max_beta = max(maximum(epmodel.beta_vec), 1e-2)
+    push!(max_beta_tracking, max_beta)
     
-    # ## SAVING DATA
-    # res_id = (:RESULT, sim_id)
-    # UJL.save_cache(res_id, (stst, βs, sim_dat); 
-    #     headline = "CATCHING RESULTS\n"
-    # )
-    
-    # ## PASSING ID TO MASTER
-    # put!(chnl, res_id)
-    
-    # GC.gc()
-    return nothing
-end # map(Rd.ststs) do stst
+    errs_len = length(errs_tracking)
+
+    try_plotting = errs_len > 1 && rem(errs_len, plot_frec) == 0.0
+    try_plotting && plot_progress(sim_id, stst, method, 
+        errs_tracking, max_beta_tracking
+    )
+
+    return (false, nothing)
+end
+
+## ----------------------------------------------------------------------------
+INDEX_CH = RemoteChannel(() -> Channel{Any}(Inf))
+
+## ----------------------------------------------------------------------------
+# ME_BOUNDED
+let
+    ststs = Rd.ststs
+    pmap(ststs) do stst
+
+        method = ME_BOUNDED
+        # This determine how often EP results will be cached
+        epochlen = 30
+        
+        epconv_kwargs = Dict()
+        epconv_kwargs[:maxiter] = Int(5e3) 
+        epconv_kwargs[:epsconv] = 1e-5
+        epconv_kwargs[:damp] = 0.9
+        epconv_kwargs[:maxvar] = 1e50
+        epconv_kwargs[:minvar] = 1e-50
+
+        epmodel_kwargs = Dict()
+        epmodel_kwargs[:alpha] = Inf
+
+        sim_id = (stst, method, FILE_ID)
+        
+        ## ----------------------------------------------------------------------------
+        # Tracking err
+        plot_frec = 50
+        errs_tracking = []
+        max_beta_tracking = []
+        epconv_kwargs[:oniter] = (it, epmodel) -> 
+            oniter(it, epmodel, sim_id, stst, method, errs_tracking, max_beta_tracking, plot_frec)
+
+        function get_model() 
+            model = load_model(:scaled_fva_pp_model, stst)
+            exp_μ = Rd.val(:μ, stst)
+            ChU.bounds!(model, M.BIOMASS_IDER, exp_μ * 0.95, exp_μ * 1.05)
+            return model
+        end
+
+        sim_dat = ChSU.cached_simulation(;
+            sim_id, epochlen, get_model,
+            verbose = true,
+            objider = M.BIOMASS_IDER, 
+            costider = M.COST_IDER,
+            clear_cache = false, use_seed = true,
+            epmodel_kwargs, epconv_kwargs
+        )
+
+        UJL.tagprintln_inmw("SAVING SIM DAT ", 
+            "\nsim id:        ", sim_id, 
+        )
+        dfile = dat_file("me_fbs_dat"; stst, method)
+        serialize(dfile, sim_dat)
+        put!(INDEX_CH, (stst, method, dfile))
+        
+        return nothing
+    end
+end
+
+## ----------------------------------------------------------------------------
+# Collect
+UJL.tagprintln_inmw("SAVING INDEX ")
+INDEX = UJL.DictTree()
+while isready(INDEX_CH)
+    stst, method, dfile = take!(INDEX_CH)
+    INDEX[:DFILE, stst, method] = dfile
+end
+ChU.save_data(dat_file("index", "bson"), INDEX)
+
+## ----------------------------------------------------------------------------
+# # RES IDS
+# # Collect all the computed results ids for bundling
+# const chnl = RemoteChannel() do
+#     Channel{Any}(10)
+# end
+# const res_ids = []
+# const collector = @async while true
+#     id = take!(chnl)
+#     push!(res_ids, id)
+# end
 
 # ## ----------------------------------------------------------------------------
-# # COLLECTING RESULTS
-# UJL.tagprintln_inmw("COLLECTING RESULTS ")
-# sleep(1) # wait for collector to get all ids
-# bundles = Dict()
-# for id in res_ids
+# # BETA EXPLORATION SIMULATION
+# # Any of the loops can be parallelized by just 
+# # changing one of the 'map' functions
 
-#     stst, ξ, βs, model, dat = UJL.load_cache(id; verbose = false)
+# # ststs = Rd.ststs
+# ststs = ["E"] # Test
+# pmap(ststs) do stst
     
-#     # Bundle
-#     bundle = get!(bundles, stst, ChU.ChstatBundle())
+#     ## ----------------------------------------------------------------------------
+#     ## SIMULATION PARAMS
+#     βs = [0.0; UJL.logspace(1, 14, 100)] 
 
-#     bundle[ξ, :net] = model
-#     bundle[ξ, :ChLP.fba] = dat[:ChLP.fba]
+#     # This determine how often EP results will be cached
+#     epochlen = Int(1e8)
+#     epconv_kwargs = Dict()
+#     # The maximum number of iteration before EP to return, even if not converged
+#     epconv_kwargs[:maxiter] = Int(3e2) 
+#     # The error threshold of convergence (Big at first)
+#     epconv_kwargs[:epsconv] = 1e-5
+#     epconv_kwargs[:damp] = 0.9
+#     epconv_kwargs[:maxvar] = 1e50
+#     epconv_kwargs[:minvar] = 1e-50
 
-#     for (βi, β) in βs |> enumerate
-#         bundle[ξ, β, :ep] = dat[(:ep, βi)]
+#     epmodel_kwargs = Dict()
+#     epmodel_kwargs[:alpha] = Inf
+
+#     # STOP CONDITION
+#     comp_model = load_model(:scaled_fva_pp_model, stst; compressed = true)
+#     exp_μ = Rd.val(:μ, stst)
+#     μ_convth = 1e-2
+
+#     ## ----------------------------------------------------------------------------
+#     # Tracking err
+#     save_frec = 50
+#     errs_tracking = []
+#     max_beta_tracking = []
+#     FIG_DIR = joinpath(M.MODEL_FIGURES_DATA_DIR, "4_err_progress")
+#     mkpath(FIG_DIR)
+#     function plot_progress()
+#         try
+#             errs_len = length(errs_tracking)
+#             curr_err = last(errs_tracking)
+
+#             UJL.tagprintln_inmw("PLOTTING", 
+#                 "\nsim id:                      ", sim_id, 
+#                 "\nerrs_len:                    ", errs_len, 
+#                 "\n"
+#             )
+
+#             p1 = plot(log10.(errs_tracking); title = "EP error evolution",
+#                 label = "", xlabel = "ep iteration", ylabel = "log(err)",
+#                 lw = 3
+#             )
+#             p2 = plot(log10.(max_beta_tracking); title = "EP beta evolution",
+#                 label = "", xlabel = "ep iteration", ylabel = "log(beta + eps)",
+#                 lw = 3
+#             )
+
+#             ps = Plots.Plot[p1, p2]
+#             layout = (2, 1)
+#             UJL.mysavefig(ps, "err_evolution", FIG_DIR; layout, stst, errs_len)
+#             UJL.mysavefig(ps, "tot_err_evolution", FIG_DIR; layout, stst)
+
+#         catch err; @warn("ERROR", stst, err); println() end
 #     end
 
-# end
+#     function oniter(it, epmodel)
 
-# ## ----------------------------------------------------------------------------
-# # SAVING
-# UJL.tagprintln_inmw("SAVING RESULTS ")
-# UJL.save_data(M.MAXENT_FBA_EB_BUNDLES_FILE, bundles)
+#         # Collect
+#         push!(errs_tracking, epmodel.stat[:max_err])
+#         max_beta = max(maximum(epmodel.beta_vec), 1e-2)
+#         push!(max_beta_tracking, max_beta)
+        
+#         errs_len = length(errs_tracking)
+    
+#         try_plotting = errs_len > 1 && rem(errs_len, save_frec) == 0.0
+#         try_plotting && plot_progress()
 
-# ## ----------------------------------------------------------------------------
-# ## CLEAR CACHE (WARNING)
-# if finish_clear_flag
-#     UJL.tagprintln_inmw("CLEARING CACHE ")
-#     UJL.delete_temp_caches()
-# end
+#         return (false, nothing)
+#     end
+#     epconv_kwargs[:oniter] = oniter
+
+#     ## ----------------------------------------------------------------------------
+#     # BETAS
+#     sim_id = (stst, FILE_ID)
+#     get_model() = load_model(:scaled_fva_pp_model, stst)
+
+#     function on_betaiter(epout)
+#         isnothing(epout) && return (false, epout)
+        
+#         curr_μ = ChU.av(comp_model, epout, M.BIOMASS_IDER)
+#         isnan(curr_μ) && error("curr_μ = NaN")
+        
+#         err = abs(curr_μ - exp_μ)/ exp_μ
+#         break_ = err < μ_convth || curr_μ > exp_μ
+
+#         UJL.tagprintln_inmw("BEFORE EPOCH", 
+#             "\nsim id:                      ", sim_id, 
+#             "\ncurr_μ:                      ", curr_μ, 
+#             "\nexp_μ:                       ", exp_μ, 
+#             "\ngrowth err:                  ", err,
+#             "\nbreak:                         ", break_,
+#             "\n"
+#         )
+        
+#         return break_
+#     end
+
+#     sim_dat = ChSU.cached_simulation(;
+#         sim_id, epochlen, 
+#         verbose = true,
+#         get_model, on_betaiter,
+#         objider = M.BIOMASS_IDER, 
+#         costider = M.COST_IDER,
+#         beta_info = [(M.BIOMASS_IDER, βs)],
+#         clear_cache = false, use_seed = true,
+#         epmodel_kwargs, epconv_kwargs
+#     )
+
+#     serialize("test_sim_dat.jld", sim_dat)
+    
+#     # ## SAVING DATA
+#     # res_id = (:RESULT, sim_id)
+#     # UJL.save_cache(res_id, (stst, βs, sim_dat); 
+#     #     headline = "CATCHING RESULTS\n"
+#     # )
+    
+#     # ## PASSING ID TO MASTER
+#     # put!(chnl, res_id)
+    
+#     # GC.gc()
+#     return nothing
+# end # map(Rd.ststs) do stst
+
+# # ## ----------------------------------------------------------------------------
+# # # COLLECTING RESULTS
+# # UJL.tagprintln_inmw("COLLECTING RESULTS ")
+# # sleep(1) # wait for collector to get all ids
+# # bundles = Dict()
+# # for id in res_ids
+
+# #     stst, ξ, βs, model, dat = UJL.load_cache(id; verbose = false)
+    
+# #     # Bundle
+# #     bundle = get!(bundles, stst, ChU.ChstatBundle())
+
+# #     bundle[ξ, :net] = model
+# #     bundle[ξ, :ChLP.fba] = dat[:ChLP.fba]
+
+# #     for (βi, β) in βs |> enumerate
+# #         bundle[ξ, β, :ep] = dat[(:ep, βi)]
+# #     end
+
+# # end
+
+# # ## ----------------------------------------------------------------------------
+# # # SAVING
+# # 
+# # UJL.save_data(M.MAXENT_FBA_EB_BUNDLES_FILE, bundles)
+
+# # ## ----------------------------------------------------------------------------
+# # ## CLEAR CACHE (WARNING)
+# # if finish_clear_flag
+# #     UJL.tagprintln_inmw("CLEARING CACHE ")
+# #     UJL.delete_temp_caches()
+# # end
